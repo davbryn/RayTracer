6066a672ef68b5dbfae5e6d1be5b0cff0c158c9c
Bryn Davies <bryn@autosonshow.tv>
2021-07-02 12:03:06 +0100

    GitIgnoe, Babel and unit tests

---
+++ src/maths/point.js
@@ -0,0 +1,8 @@
+class Point {
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = 1.0;
+    }
+}
---
+++ src/maths/tuple.js
@@ -0,0 +1,11 @@
+'use strict';
+
+export default class Tuple {
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = 0.0;
+    }
+}
+
